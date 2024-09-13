72efde8b5f66e5f48991caf1d6cfdaf89a810055
eduardo moro <dev.eduardomoro@gmail.com>
2024-09-12 23:05:11 -0300

    enable vercel insights

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 
 const app = express();
+
+inject();
+injectSpeedInsights();
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
