070952cff0a823c522700215c072da2fa4f7edc5
eduardo moro <eduardomoro1127@gmail.com>
2024-09-12 23:15:47 -0300

    added vercel monitoring to html

--- api/index.js
+++ api/index.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-import { inject } from "@vercel/analytics"
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
 
 const app = express();
 
-inject();
-injectSpeedInsights();
-
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-module.exports = (req, res) => {
+export default async (req, res) => {
     app(req, res);
 };
